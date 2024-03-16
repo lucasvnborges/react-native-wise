1dfb4ec29055ae96b8aa690ef32c1e5513c87b33
Lucas Borges <lucasvnborges@gmail.com>
2024-03-16 20:12:34 -0300

    initial exchange rate component

--- app/screens/home/components/ExchangeRate.tsx
+++ app/screens/home/components/ExchangeRate.tsx
@@ -2,21 +2,19 @@ import React from 'react'
 import styled from 'styled-components/native'
 import { Text, useTheme } from 'react-native-paper'
 import { SmallButton } from '../../../components/Paper'
+import { View } from 'react-native'
 
 const ExchangeRateContainer = styled.View`
   margin: 16px;
   height: 300px;
   padding: 20px;
   border-radius: 20px;
-  flex-direction: row;
   background-color: ${({ theme }) => theme.colors.surfaceVariant};
 `
 
 const ExchangeRateInfo = styled.View`
   flex: 1;
   height: 40px;
-  align-items: center;
-  flex-direction: row;
   justify-content: space-between;
 `
 
@@ -25,33 +23,41 @@ const ExchangeRate = () => {
 
   return (
     <ExchangeRateContainer>
-      <ExchangeRateInfo>
-        <Text style={{ fontFamily: theme.fonts.semibold, fontSize: 20 }}>
-          1 AUD = 3.30 BRL
-        </Text>
-        <Text style={{ fontFamily: theme.fonts.regular, fontSize: 16 }}>
-          Today
-        </Text>
-      </ExchangeRateInfo>
-      <SmallButton
-        compact
-        mode="contained"
-        onPress={console.log}
-        style={{ marginRight: 12 }}
-        textColor={theme.colors.onPrimaryContainer}
-        buttonColor={theme.colors.primaryContainer}
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
       >
-        <Text
-          style={{
-            height: 21,
-            fontSize: 16,
-            alignSelf: 'center',
-            fontFamily: 'semibold',
-          }}
+        <ExchangeRateInfo>
+          <Text style={{ fontFamily: 'semibold', fontSize: 20 }}>
+            1 AUD = 3.30 BRL
+          </Text>
+          <Text style={{ fontFamily: 'regular', fontSize: 16 }}>Today</Text>
+        </ExchangeRateInfo>
+        <SmallButton
+          compact
+          mode="contained"
+          onPress={console.log}
+          style={{ marginRight: 12 }}
+          textColor={theme.colors.onPrimaryContainer}
+          buttonColor={theme.colors.primaryContainer}
         >
-          Edit
-        </Text>
-      </SmallButton>
+          <Text
+            style={{
+              height: 21,
+              fontSize: 16,
+              alignSelf: 'center',
+              fontFamily: 'semibold',
+            }}
+          >
+            Edit
+          </Text>
+        </SmallButton>
+      </View>
+
+      <View style={{ flex: 1 }}></View>
     </ExchangeRateContainer>
   )
 }
