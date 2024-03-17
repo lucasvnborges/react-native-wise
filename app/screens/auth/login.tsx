b8447589ba2eb49b5ed51b9f63ef30bfd27b9c2f
Lucas Borges <lucasvnborges@gmail.com>
2024-03-17 13:18:17 -0300

    adjusts the login screen with the new components

--- app/screens/auth/login.tsx
+++ app/screens/auth/login.tsx
@@ -1,11 +1,13 @@
+import styled from 'styled-components/native'
 import React, { useCallback, useEffect, useRef, useState } from 'react'
-import { View, StyleSheet, Keyboard, ScrollView } from 'react-native'
+import { View, Keyboard, ScrollView } from 'react-native'
 import { Realm, useApp } from '@realm/react'
-import { Text, TextInput, useTheme, IconButton } from 'react-native-paper'
-import { Button } from '../../components/Paper'
-import { StatusBar } from 'expo-status-bar'
-import { useNavigation } from '@react-navigation/native'
+import { Text, useTheme, IconButton } from 'react-native-paper'
+import { NavigationProp } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { AuthStackParamList } from '../../navigation'
+import { Button } from '../../components'
+import { TextInput } from '../../components/Paper'
 
 export enum AuthState {
   None,
@@ -14,12 +16,65 @@ export enum AuthState {
   RegisterError,
 }
 
-const LoginScreen = () => {
+type Props = {
+  navigation: NavigationProp<AuthStackParamList>
+}
+
+const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`
+
+const Header = styled(View)`
+  height: 54px;
+  padding-horizontal: 12px;
+`
+
+const Title = styled(Text)`
+  font-size: 30px;
+  padding-vertical: 12px;
+  padding-horizontal: 24px;
+  font-family: 'semibold';
+`
+
+const FormContainer = styled(View)`
+  flex: 1;
+  padding: 12px;
+  padding-vertical: 32px;
+`
+
+const InputContainer = styled(View)`
+  padding-vertical: 8px;
+  padding-horizontal: 12px;
+`
+
+const ErrorText = styled(Text)`
+  color: #f66;
+  font-size: 12px;
+  margin-top: 12px;
+  text-align: center;
+`
+
+const ButtonContainer = styled(View)`
+  margin-top: 12px;
+  padding-horizontal: 12px;
+`
+
+const StyledButton = styled(Button)`
+  margin-top: 12px;
+`
+
+export default function LoginScreen({ navigation }: Props) {
+  // hooks
   const theme = useTheme()
-  const navigation = useNavigation()
-  const app = useApp()
+
+  // realm
+  const { logIn } = useApp()
+
+  // ref
   const refPassword = useRef(null)
 
+  // state
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [authState, setAuthState] = useState(AuthState.None)
@@ -50,181 +105,121 @@ const LoginScreen = () => {
     setAuthState(AuthState.Loading)
     const credentials = Realm.Credentials.emailPassword(email, password)
     try {
-      await app.logIn(credentials)
+      await logIn(credentials)
       setAuthState(AuthState.None)
     } catch (e) {
       setAuthState(AuthState.LoginError)
     }
-  }, [email, password, setAuthState, app])
+  }, [email, password, setAuthState, logIn])
 
   return (
-    <SafeAreaView
-      testID="loginScreen"
-      style={[styles.container, { backgroundColor: theme.colors.background }]}
-    >
-      <StatusBar style="dark" backgroundColor="#fff" />
-
-      <View>
-        <View style={{ height: 54, paddingHorizontal: 12 }}>
-          <IconButton
-            size={26}
-            mode="contained"
-            icon="arrow-left"
-            testID="loginScreenBackButton"
-            iconColor={theme.colors.primary}
-            onPress={() => navigation.goBack()}
-          />
-        </View>
-
-        <View style={{ paddingHorizontal: 24, paddingVertical: 12 }}>
-          <Text
-            style={{ fontFamily: 'semibold', fontSize: 30 }}
-            testID="loginScreenScreenTitle"
-          >
-            Login
-          </Text>
-        </View>
-      </View>
-
+    <Container testID="loginScreen">
+      <Header>
+        <IconButton
+          size={26}
+          mode="contained"
+          icon="arrow-left"
+          testID="loginScreenBackButton"
+          iconColor={theme.colors.primary}
+          onPress={() => navigation.goBack()}
+        />
+      </Header>
+      <Title testID="loginScreenScreenTitle">Log in</Title>
       <ScrollView
         testID="loginScreenScrollView"
         keyboardShouldPersistTaps="always"
-        contentContainerStyle={styles.form}
         showsVerticalScrollIndicator={false}
-        style={{ flex: 1, backgroundColor: theme.colors.background }}
+        contentContainerStyle={{ flexGrow: 1 }}
       >
-        <View style={styles.inputContainer}>
-          <Text
-            testID="loginScreenEmailLabel"
-            style={{ fontFamily: 'regular', marginBottom: 4 }}
-          >
-            Email
-          </Text>
-          <TextInput
-            label=""
-            autoFocus
-            value={email}
-            mode="outlined"
-            autoCorrect={false}
-            blurOnSubmit={false}
-            returnKeyType="next"
-            autoCapitalize="none"
-            onChangeText={setEmail}
-            testID="loginScreenEmailInput"
-            textContentType="emailAddress"
-            outlineStyle={{ borderRadius: 12 }}
-            error={
-              authState === AuthState.LoginError ||
-              authState === AuthState.RegisterError
-            }
-            onSubmitEditing={() => refPassword.current?.focus()}
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text
-            testID="loginScreenPasswordLabel"
-            style={{ fontFamily: 'regular', marginBottom: 4 }}
-          >
-            Password
-          </Text>
-          <TextInput
-            label=""
-            mode="outlined"
-            value={password}
-            ref={refPassword}
-            textContentType="password"
-            onChangeText={setPassword}
-            testID="loginScreenPasswordInput"
-            outlineStyle={{ borderRadius: 12 }}
-            secureTextEntry={secureTextEntry}
-            error={
-              authState === AuthState.LoginError ||
-              authState === AuthState.RegisterError
-            }
-            right={
-              <TextInput.Icon
-                testID="loginScreenButtonShowPassword"
-                onPress={() => setSecureTextEntry(!secureTextEntry)}
-                icon={secureTextEntry ? 'eye-off-outline' : 'eye-outline'}
-              />
-            }
-          />
-        </View>
-
-        {authState === AuthState.LoginError && (
-          <Text
-            style={[styles.error]}
-            testID="loginScreenCredentialsErrorMessage"
-          >
-            Make sure the email and password are correct
-          </Text>
-        )}
-
-        <View style={{ paddingHorizontal: 12, marginTop: 12 }}>
-          <Button
-            mode="contained"
-            testID="loginScreenButton"
-            onPress={handleLogin}
-            style={styles.buttonStyle}
-            contentStyle={styles.buttonContentStyle}
-            disabled={authState === AuthState.Loading}
-            textColor={theme.colors.onPrimaryContainer}
-            buttonColor={theme.colors.primaryContainer}
-          >
-            Log In
-          </Button>
-          <Button
-            underline
-            mode="text"
-            onPress={console.log}
-            style={styles.buttonStyle}
-            testID="loginScreenRecoveryButton"
-            contentStyle={styles.buttonContentStyle}
-            disabled={authState === AuthState.Loading}
-            textColor={theme.colors.onPrimaryContainer}
-          >
-            Trouble logging in?
-          </Button>
-        </View>
+        <FormContainer>
+          <InputContainer>
+            <Text
+              testID="loginScreenEmailLabel"
+              style={{ fontFamily: 'regular', marginBottom: 4 }}
+            >
+              Email
+            </Text>
+            <TextInput
+              label=""
+              autoFocus
+              value={email}
+              mode="outlined"
+              autoCorrect={false}
+              blurOnSubmit={false}
+              returnKeyType="next"
+              autoCapitalize="none"
+              onChangeText={setEmail}
+              testID="loginScreenEmailInput"
+              textContentType="emailAddress"
+              outlineStyle={{ borderRadius: 12 }}
+              error={
+                authState === AuthState.LoginError ||
+                authState === AuthState.RegisterError
+              }
+              onSubmitEditing={() => refPassword.current?.focus()}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Text
+              testID="loginScreenPasswordLabel"
+              style={{ fontFamily: 'regular', marginBottom: 4 }}
+            >
+              Password
+            </Text>
+            <TextInput
+              label=""
+              mode="outlined"
+              value={password}
+              ref={refPassword}
+              textContentType="password"
+              onChangeText={setPassword}
+              testID="loginScreenPasswordInput"
+              outlineStyle={{ borderRadius: 12 }}
+              secureTextEntry={secureTextEntry}
+              error={
+                authState === AuthState.LoginError ||
+                authState === AuthState.RegisterError
+              }
+              right={
+                <TextInput.Icon
+                  testID="loginScreenButtonShowPassword"
+                  onPress={() => setSecureTextEntry(!secureTextEntry)}
+                  icon={secureTextEntry ? 'eye-off-outline' : 'eye-outline'}
+                />
+              }
+            />
+          </InputContainer>
+          {authState === AuthState.LoginError && (
+            <ErrorText testID="loginScreenCredentialsErrorMessage">
+              Make sure the email and password are correct
+            </ErrorText>
+          )}
+          <ButtonContainer>
+            <StyledButton
+              rounded
+              fontSize={18}
+              mode="contained"
+              testID="loginScreenButton"
+              onPress={handleLogin}
+              disabled={authState === AuthState.Loading}
+              textColor={theme.colors.onPrimaryContainer}
+              buttonColor={theme.colors.primaryContainer}
+            >
+              Log In
+            </StyledButton>
+            <StyledButton
+              underline
+              fontSize={18}
+              onPress={console.log}
+              testID="loginScreenRecoveryButton"
+              disabled={authState === AuthState.Loading}
+              textColor={theme.colors.onPrimaryContainer}
+            >
+              Trouble logging in?
+            </StyledButton>
+          </ButtonContainer>
+        </FormContainer>
       </ScrollView>
-    </SafeAreaView>
+    </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  form: {
-    flex: 1,
-    padding: 12,
-    paddingVertical: 32,
-    // justifyContent: 'center',
-  },
-  inputContainer: {
-    paddingVertical: 8,
-    paddingHorizontal: 12,
-  },
-  error: {
-    fontSize: 12,
-    marginTop: 12,
-    color: '#f66',
-    textAlign: 'center',
-  },
-  image: {
-    flex: 1,
-    padding: 64,
-    maxHeight: 300,
-  },
-  buttonStyle: {
-    marginTop: 12,
-  },
-  buttonContentStyle: {
-    height: 48,
-  },
-  labelStyle: {
-    fontSize: 16,
-  },
-})
-
-export default LoginScreen
