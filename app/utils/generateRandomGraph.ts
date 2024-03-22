12da8dd1a71f815c281c63094214e4ec4c2a2041
Lucas Borges <lucasvnborges@gmail.com>
2024-03-22 17:40:58 -0300

    gaussian function to generate mock graph values

--- app/utils/generateRandomGraph.ts
+++ app/utils/generateRandomGraph.ts
@@ -1,18 +1,28 @@
 import gaussian from 'gaussian'
+import type { GraphPoint } from 'react-native-graph'
 
-export function weightedRandom(mean: number, variance: number): number {
+export function weightedRandom(
+  mean: number,
+  variance: number,
+  min: number,
+  max: number,
+): number {
   var distribution = gaussian(mean, variance)
-  // Take a random sample using inverse transform sampling method.
-  return distribution.ppf(Math.random())
+  var randomValue
+  // Ajuste para o intervalo [min, max] e formate para quatro casas decimais
+  do {
+    randomValue = distribution.ppf(Math.random())
+  } while (randomValue < min || randomValue > max)
+  return parseFloat(randomValue.toFixed(4))
 }
 
-export function generateRandomGraphData(length: number): any {
+export function generateRandomGraphData(length: number): GraphPoint[] {
   return Array<number>(length)
     .fill(0)
     .map((_, index) => ({
       date: new Date(
         new Date(2000, 0, 1).getTime() + 1000 * 60 * 60 * 24 * index,
       ),
-      value: (Math.random() * 0.35 + 3.1).toFixed(4),
+      value: weightedRandom(3.25, Math.pow(index + 1, 2), 3.001, 3.499),
     }))
 }
