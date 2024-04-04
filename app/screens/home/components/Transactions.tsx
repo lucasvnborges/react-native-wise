7ebc809076b1d10b742161690cd5ac00b76c55b1
Lucas Borges <lucasvnborges@gmail.com>
2024-04-03 21:42:46 -0300

    add mock

--- app/screens/home/components/Transactions.tsx
+++ app/screens/home/components/Transactions.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+const data = [
+  {
+    sender: 'Lucas Borges',
+    type: 'Paid',
+    date: '2024-04-03',
+    value: 50
+  }
+]
+
 export default function Transactions() {
   return (
     <View>
