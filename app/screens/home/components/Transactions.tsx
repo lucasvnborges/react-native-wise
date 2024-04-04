c56f6aaab7a132d1ad9b89dfe07d718dc3fe311f
Lucas Borges <lucasvnborges@gmail.com>
2024-04-03 21:56:54 -0300

    add flatlist to transactions component

--- app/screens/home/components/Transactions.tsx
+++ app/screens/home/components/Transactions.tsx
@@ -1,28 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 
 const data = [
   {
+    id: 1,
     sender: 'Lucas Borges',
     type: 'Paid',
     date: '2024-04-03',
-    value: 50
-  }
+    value: 50,
+  },
 ]
 
 type RenderItemProps = {
-  item: any;
+  item: any
 }
 
 export default function Transactions() {
-  
-  const renderItem = ({ item }: RenderItemProps) => {
-
-  }
+  const renderItem = ({ item }: RenderItemProps) => {}
 
   return (
     <View>
       <Text>Transactions</Text>
+
+      <FlatList
+        data={data}
+        decelerationRate="fast"
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   )
 }
